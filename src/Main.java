304cd35cbd3241e3cfaa653883d539102e8095b6
mehmet akkaya <mehmetakkaya1990@gmail.com>
2022-07-20 16:54:55 +0300

    Revert "değişiklik yapıldı"
    This reverts commit a74e9d23b83ef3278cb5c40916171aa4bea9410a.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //değişkenlerimizi oluşturalım
-        int a, b;
-        double c;
+        double tutar, kdvOran = 0.18, kdvTutar, kdvliTutar;
+        Scanner input = new Scanner(System.in);
+        System.out.print("ücret tutarını giriniz : ");
+        tutar = input.nextDouble();
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+        System.out.println("kdvsiztutar : " + tutar);
+        System.out.println("kdvoranı : " + kdvOran);
+        System.out.println("kdvtutarı : " + kdvTutar);
+        System.out.println("kdvtoplamı : " + kdvliTutar);
 
-        //kullanıcıdan verilerimizi alalım
 
-        Scanner girdi = new Scanner(System.in);
-        System.out.print("1. kenarı giriniz : ");
-        a = girdi.nextInt();
-        System.out.print("2. kenarı giriniz : ");
-        b = girdi.nextInt();
-
-        c = Math.sqrt((a*a) + (b*b));
-        System.out.println("hipotenüs : " + c);
     }
 }
